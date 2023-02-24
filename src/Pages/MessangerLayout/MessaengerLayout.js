c2cc87437dfae49a2707693a87051e493de58ed5
Sharif-Miah <farhansharif141077@gmail.com>
2023-02-24 21:50:31 +0600

    side navbar icon added

--- src/Pages/MessangerLayout/MessaengerLayout.js
+++ src/Pages/MessangerLayout/MessaengerLayout.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {CiUser} from 'react-icons/ci'
+import {CiUser} from 'react-icons/ci';
+import {AiOutlineUsergroupAdd} from 'react-icons/ai';
+import {AiOutlineHome} from 'react-icons/ai';
+import {AiOutlineMail} from 'react-icons/ai';
+import {BiShoppingBag} from 'react-icons/bi';
+import {AiOutlineSetting} from 'react-icons/ai';
+import {CgProfile} from 'react-icons/cg';
+import {HiOutlineLogout} from 'react-icons/hi';
 
 import NavBar from './NavBar';
 
@@ -12,20 +19,22 @@ const MessaengerLayout = () => {
             <div className='grid grid-cols-12'>
                 <div class="flex flex-row h-screen w-screen	 bg-gray-100" >
                     <div class="flex flex-col justify-between items-center flex-none w-16 bg-gray-200">
-                        <div class="flex flex-col space-y-4 w-full items-center pt-5">
-                            <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+                        <div class="flex flex-col space-y-4 w-full items-center pt-7">
+                            <a><div title='Dashbord' class="rounded-full bg-gray-400 w-8 h-8 cursor-pointer"> <AiOutlineHome className='text-2xl m-auto pt-1'/></div></a>
                             <a class="block relative w-full h-16 w-16 flex justify-center items-center">
-                                <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path></svg>
+                                <div title='Massage' className='rounded-full bg-gray-400 w-8 h-8 cursor-pointer'>
+                                <AiOutlineMail className="w-6 h-6 text-2xl m-auto pt-1"/>
+                                </div>
                                 <div class="absolute top-0 right-0 mr-3 mt-3 bg-red-500 w-4 h-4 text-xs text-white rounded-full text-center">5</div>
                             </a>
-                            <a><div > <CiUser class="rounded-full bg-gray-400 w-8 h-8"/></div></a>
-                            <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-                            <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-                            <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+                            <a><div title='Users' className='rounded-full bg-gray-400 w-8 h-8 cursor-pointer text-black'><CiUser class=" text-2xl m-auto pt-1"/></div></a>
+                            <a><div title='Group' class="rounded-full bg-gray-400 w-8 h-8 cursor-pointer"><AiOutlineUsergroupAdd className='text-2xl m-auto pt-1'/></div></a>
+                            <a><div title='Shoping' class="rounded-full bg-gray-400 w-8 h-8 cursor-pointer"><BiShoppingBag className='text-2xl m-auto pt-1'/></div></a>
+                            <a><div title='Setting'  class="rounded-full bg-gray-400 w-8 h-8 cursor-pointer"><AiOutlineSetting className='text-2xl m-auto pt-1'/></div></a>
                         </div>
                         <div class="flex flex-col space-y-4 pb-5">
-                            <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-                            <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+                            <a><div title='profile' class="rounded-full bg-gray-400 w-8 h-8 cursor-pointer"><CgProfile className='text-3xl m-auto pt-1'/></div></a>
+                            <a><div title='LogOut' class="rounded-full bg-gray-400 w-8 h-8 cursor-pointer"><HiOutlineLogout className='text-2xl m-auto pt-1'/></div></a>
                         </div>
 
                     </div>
